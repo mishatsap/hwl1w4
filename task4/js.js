cf7e533e982fca8854d3a8be3a21caa29e98dc01
Misha Tsap <tsapmisha@gmail.com>
2018-08-22 11:36:58 +0300

    reworked the map

--- task4/js.js
+++ task4/js.js
@@ -1,59 +1,18 @@
+function initMap() {
+	var element = document.getElementById("map");
+	var options = {
+		zoom: 20,
+		center: {lat: 49.8393088, lng: 24.0336896}
+	};
 
-window.onload = function() {
-	var result;
-	var map;
-	var marker;
+	var myMap = new google.maps.Map(element, options);
 
-  result = document.getElementById('result');
+	var marker = new google.maps.Marker({
+		position: {lat: 49.8393088, lng: 24.0336896},
+		map: myMap
+	});
 
-  var ourOptions = {
-  	zoom: 17,
-  	mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-
-  map = new google.maps.Map(document.getElementById('mapSurface'), ourOptions);
-  function newMarker(position) {
-  	var myPos = {
-  	  lat: position.coords.latitude,
-  	  lng: position.coords.longitude
-    };
-
-    marker = new google.maps.Marker({
-  	  position: myPos,
-      map: map,
-      icon: 'marker.png'
-    });
-
-    marker.setMap(map);
-  }
-  
-
-  if(navigator.geolocation) {
-  	navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationFailure);
-
-  	result.innerHTML = 'My map';
-  } else {
-  	result.innerHTML = 'Браузер не підтримує геолокацію';
-
-  	goToDefultLocation();
-  }
-
-  function geolocationSuccess(position) {
-  	var location = new 
-  	google.maps.LatLng(position.coords.latitude,
-  	position.coords.longitude);
-
-  	map.setCenter(location);
-  }
-
-  function geolocationFailure() {
-  	goToDefultLocation();
-  }
-
-  function goToDefultLocation() {
-  	var newYork = new google.maps.LatLng(40.730610, -73.935242);
-
-  	map.setCenter(newYork);
-  }
-}
-
+	var InfoWindow = new google.maps.InfoWindow({
+		content: ""
+	});
+};
