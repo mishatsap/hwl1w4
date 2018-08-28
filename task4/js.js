3e8e1284360cc4ddf63434b13dd9cce77fc039e7
Misha Tsap <40633610+mishatsap@users.noreply.github.com>
2018-08-28 11:25:28 +0300

    reworked the map

--- task4/js.js
+++ task4/js.js
@@ -1,14 +1,40 @@
-function initMap() {
-	var element = document.getElementById("map");
-	var options = {
-		zoom: 20,
-		center: {lat: 49.8393088, lng: 24.0336896}
-	};
+var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 49.8393088, lng: 24.0336896},
+          zoom: 22
+        });
+        infoWindow = new google.maps.InfoWindow;
 
-	var myMap = new google.maps.Map(element, options);
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
 
-	var marker = new google.maps.Marker({
-		position: {lat: 49.8393088, lng: 24.0336896},
-		map: myMap
-	});
-};
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Hello!');
+            infoWindow.open(map);
+            map.setCenter(pos);
+
+            const marker = new google.maps.Marker({
+				position: pos,
+				map: map,
+				title: 'Hello!'
+			});
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
